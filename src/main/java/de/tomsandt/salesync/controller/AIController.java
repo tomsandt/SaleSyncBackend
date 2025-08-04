6ab1bcc924f01e08f8a09526bf8d53ae69b73afe
Tom <tom.sandt@telekom.de>
2025-08-04 10:04:05 +0200

    added AI testing

--- src/main/java/de/tomsandt/salesync/controller/AIController.java
+++ src/main/java/de/tomsandt/salesync/controller/AIController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/ai")
+@RequestMapping("/ai")
 @CrossOrigin(origins =  "*")
 public class AIController {
 
